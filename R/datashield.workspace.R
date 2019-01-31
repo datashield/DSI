812cb7227d7f7de7aad7a141c863acc2ade1ed03
Yannick Marcon <yannick.marcon@obiba.org>
2019-01-31 21:42:27 +0100

    More fun with workspaces

--- R/datashield.workspace.R
+++ R/datashield.workspace.R
@@ -6,25 +6,64 @@
 #'
 #' @export
 datashield.workspaces <- function(conns) {
-  res <- lapply(conns, FUN=dsListWorkspaces)
-  server <- c()
-  name <- c()
-  user <- c()
-  context <- c()
-  lastAccessDate <- c()
-  size <- c()
-  for (n in names(res)) {
-    wss <- res[[n]]
-    if (!is.character(wss)) {
-      server <- c(server, as.vector(wss$server))
-      name <- c(name, as.vector(wss$name))
-      user <- c(user, as.vector(wss$user))
-      context <- c(context, as.vector(wss$context))
-      lastAccessDate <- c(lastAccessDate, as.vector(wss$lastAccessDate))
-      size <- c(size, as.vector(wss$size))
+  if (is.list(conns)) {
+    res <- lapply(conns, FUN=dsListWorkspaces)
+    server <- c()
+    name <- c()
+    user <- c()
+    context <- c()
+    lastAccessDate <- c()
+    size <- c()
+    for (n in names(res)) {
+      wss <- res[[n]]
+      if (!is.character(wss)) {
+        server <- c(server, as.vector(wss$server))
+        name <- c(name, as.vector(wss$name))
+        user <- c(user, as.vector(wss$user))
+        context <- c(context, as.vector(wss$context))
+        lastAccessDate <- c(lastAccessDate, as.vector(wss$lastAccessDate))
+        size <- c(size, as.vector(wss$size))
+      }
     }
+    if (length(server)) {
+      data.frame(server=server, name=name, user=user, context=context, lastAccessDate=lastAccessDate, size=size)
+    } else {
+      data.frame()
+    }
+  } else {
+    dsListWorkspaces(conns)
   }
-  if (length(server)) {
-    data.frame(server=server, name=name, user=user, context=context, lastAccessDate=lastAccessDate, size=size)
+}
+
+#' Save DataSHIELD R session to a workspace
+#'
+#' Save the current state of the DataSHIELD R session in a workspace with the provided name in each data repository.
+#' The workspace can be restored on the next \code{\link{datashield.login}}.
+#'
+#' @param conns \code{\link{DSConnection-class}} object or a list of \code{\link{DSConnection-class}}s.
+#' @param ws The workspace name
+#' @export
+datashield.workspace_save <- function(conns, ws) {
+  if (is.list(conns)) {
+    ignore <- lapply(conns, FUN = datashield.workspace_save, ws)
+  } else {
+    name <- paste0(conns@name, ":", ws)
+    ignore <- dsSaveWorkspace(conns, name)
+  }
+}
+
+#' Remove a DataSHIELD workspace
+#'
+#' Remove in each data repository the workspace with the provided name.
+#'
+#' @param conns \code{\link{DSConnection-class}} object or a list of \code{\link{DSConnection-class}}s.
+#' @param ws The workspace name
+#' @export
+datashield.workspace_rm <- function(conns, ws) {
+  if (is.list(conns)) {
+    ignore <- lapply(conns, FUN = datashield.workspace_rm, ws)
+  } else {
+    name <- paste0(conns@name, ":", ws)
+    ignore <- dsRmWorkspace(conns, name)
   }
 }
