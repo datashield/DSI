4e65cbd238e57008f4f11107f759979293abe0c5
Yannick Marcon <yannick.marcon@obiba.org>
2020-05-13 23:59:12 +0200

    datashield.resources() added

--- R/datashield.list.R
+++ R/datashield.list.R
@@ -6,7 +6,7 @@
 #' @return Table unique names from all the servers.
 #' @examples
 #' \dontrun{
-#'   datashield.table(conns)
+#'   datashield.tables(conns)
 #' }
 #' @export
 datashield.tables <- function(conns) {
@@ -14,5 +14,24 @@ datashield.tables <- function(conns) {
     lapply(conns, function(c) { dsListTables(c) })
   } else {
     dsListTables(conns)
+  }
+}
+
+#' List of the resources
+#'
+#' Get the list of all the resources from the different data repositories.
+#'
+#' @param conns \code{\link{DSConnection-class}} object or a list of \code{\link{DSConnection-class}}s.
+#' @return Resource unique names from all the servers.
+#' @examples
+#' \dontrun{
+#'   datashield.resources(conns)
+#' }
+#' @export
+datashield.resources <- function(conns) {
+  if (is.list(conns)) {
+    lapply(conns, function(c) { dsListResources(c) })
+  } else {
+    dsListResources(conns)
   }
 }
